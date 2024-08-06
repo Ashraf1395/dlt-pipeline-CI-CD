88de5da8955afafdb11df21d73d3540181ccee31
Ashraf1395 <1395ashraf@gmail.com>
2024-08-06 15:03:40 +0530

    update

---
+++ notebooks/stackgenius-okta-dlt-pipeline copy.sql
@@ -0,0 +1,53 @@
+-- Databricks notebook source
+CREATE OR REFRESH STREAMING LIVE TABLE okta_silver_dlt
+COMMENT "Incremental upload of data incoming from okta"
+PARTITIONED BY (date, tenant_id)
+AS
+SELECT *
+FROM cloud_files(
+  '/Volumes/stackgenius/dev/raw_okta_data/okta_raw_json_new/*/*/*.json',
+  'json',
+  map('inferschema',"true")
+);
+
+-- COMMAND ---
+
+CREATE OR REFRESH MATERIALIZED VIEW okta_gold_dlt AS
+SELECT
+    `date`::DATE,
+    tenant_id,
+    actor_alternateId,
+    actor_displayName,
+    actor_type,
+    client_device,
+    displayMessage,
+    outcome_result,
+    outcome_reason,
+    eventType,
+    published::TIMESTAMP,
+    transaction_id,
+    CONCAT(city, ' ', country) AS location,
+    CONCAT_WS(',', COLLECT_LIST(target_exploded.alternateId)) AS target_alternateId,
+    CONCAT_WS(',', COLLECT_LIST(target_exploded.displayName)) AS target_displayName
+FROM (
+    SELECT *, 
+        get_json_object(transaction, "$.id") AS transaction_id,
+        get_json_object(client_geographicalContext, "$.city") AS city,
+        get_json_object(client_geographicalContext, "$.country") AS country,
+        EXPLODE(FROM_JSON(get_json_object(target, '$'), 'ARRAY<STRUCT<alternateId: STRING, displayName: STRING>>')) AS target_exploded
+    FROM live.okta_silver_dlt
+) AS exploded_table
+GROUP BY `date`, tenant_id, actor_alternateId, actor_displayName, actor_type, client_device, displayMessage, outcome_result, outcome_reason, eventType, published, transaction_id, city, country
+ORDER BY published DESC;
+
+CREATE OR REFRESH LIVE TABLE ingestion_metadata_dlt AS
+SELECT
+  'okta' as source,
+  tenant_id,
+  MAX(published) as most_recent_timestamp
+FROM
+  live.okta_silver_dlt
+GROUP BY
+  source,
+  tenant_id
+
